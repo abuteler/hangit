6805668282e5d0bc6562be1dd02015db104c9b2f
Kleer LA <kleer@kleer-sandbox.(none)>
2016-07-02 12:43:29 -0300

    Added life counter

--- features/Game.feature
+++ features/Game.feature
@@ -10,3 +10,13 @@ Scenario: User starts the game
 	When The user starts the game
 	Then The user should see the secret word "_ _ _ _ _ _ _"
 
+Scenario: User can track his remaining lifes
+	Given The user opens the app
+	When The user starts the game
+	Then The user sees the life counter with "5/5"
+	
+#Scenario: User loses a life
+#	Given The user opens the app
+#		And The user starts the game
+#	When The user enters the letter "z", which is not present in the secret word "bartulos"
+#	Then The user should see its life counter decreased by one
--- features/step_definitions/my_steps.rb
+++ features/step_definitions/my_steps.rb
@@ -6,6 +6,11 @@ When(/^The user starts the game$/) do
   visit '/play'
 end
 
+When(/^The user enters the letter "(.*?)", which is not present in the secret word "(.*?)"$/) do |letter, secret_word|
+  fill_in("input", :with => letter)
+  click_button("submit")
+end
+
 Then(/^The user should see a welcome message: "(.*?)"$/) do |welcome_msg|
   last_response.body.should =~ /#{welcome_msg}/m
 end
@@ -18,5 +23,12 @@ Then(/^The user should see the secret word "(.*?)"$/) do |text|
   last_response.body.should =~ /#{text}/m
 end
 
+Then(/^The user should see its life counter decreased by one$/) do
+  pending
+  last_response.body.should =~ /#{text}/m
+end
 
+Then(/^The user sees the life counter with "(.*?)"$/) do |life_cnt|
+  last_response.body.should =~ /#{life_cnt}/m
+end
 
